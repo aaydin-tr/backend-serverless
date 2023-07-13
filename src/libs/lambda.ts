cb59b9b88fbd9f222b79007c92f4fab8b33581de
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-07-13 15:50:08 +0300

    feat: auth middleware

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,10 +1,25 @@
 import middy from "@middy/core";
 import middyJsonBodyParser from "@middy/http-json-body-parser";
 import httpErrorHandlerMiddleware from "./error-handler";
+import JWTAuthMiddleware, {
+  EncryptionAlgorithms,
+} from "middy-middleware-jwt-auth";
 
+export default httpErrorHandlerMiddleware;
 
-export const middyfy = (handler) => {
-  return middy(handler)
+export const middyfy = (handler, protectedRoute: boolean = false) => {
+  const m = middy(handler)
     .use(middyJsonBodyParser())
     .use(httpErrorHandlerMiddleware());
+  if (protectedRoute) {
+    m.use(
+      JWTAuthMiddleware({
+        algorithm: EncryptionAlgorithms.HS256,
+        credentialsRequired: true,
+        secretOrPublicKey: process.env.JWT_SECRET,
+      })
+    );
+  }
+
+  return m;
 };
