9b18e1348e0a7320595cc9ea2228c1f1d926818c
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-07-13 15:49:34 +0300

    feat: add mapper for user dto

--- src/repository/user/user.ts
+++ src/repository/user/user.ts
@@ -1,12 +1,18 @@
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import User from "../../models/user";
+import { mapper } from "../../dto";
+import { Dto, UserDto } from "../../dto/user";
 
 class UserRepository {
   private readonly docClient: DocumentClient;
   private readonly tableName: string;
   private readonly uniqueTableName: string;
 
-  constructor(docClient: DocumentClient, tableName: string, uniqueTableName: string) {
+  constructor(
+    docClient: DocumentClient,
+    tableName: string,
+    uniqueTableName: string
+  ) {
     this.docClient = docClient;
     this.tableName = tableName;
     this.uniqueTableName = uniqueTableName;
@@ -21,8 +27,20 @@ class UserRepository {
         ExpressionAttributeValues: { ":email": email },
       })
       .promise();
-      
+
     return result.Items[0] as User;
+  }
+
+  async getUserById(id: string): Promise<Dto> {
+    const result = await this.docClient
+      .query({
+        TableName: this.tableName,
+        KeyConditionExpression: "id = :id",
+        ExpressionAttributeValues: { ":id": id },
+      })
+      .promise();
+
+    return mapper<Dto, User>(result.Items[0] as User, new UserDto());
   }
 
   async createUser(user: User): Promise<any> {
@@ -40,7 +58,7 @@ class UserRepository {
             Put: {
               ConditionExpression: "attribute_not_exists(id)",
               Item: {
-                id: "email#" + user.email,
+                id: "id#" + user.id,
                 email: user.email,
               },
               TableName: this.uniqueTableName,
